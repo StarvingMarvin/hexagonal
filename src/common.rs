6996dba0f63acd6b0584b769901cf995c1ed8574
Luka Stojanovic <luka@magrathea.rs>
2023-11-13 19:41:04 +0100

    Hexagonal Error and Result types

--- src/common.rs
+++ src/common.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, fmt::Display};
+
 #[derive(Hash, PartialEq, Eq, Copy, Clone, Debug)]
 pub enum Player {
     Black,
@@ -19,3 +21,25 @@ impl From<Player> for usize {
     }
 }
 
+#[derive(Debug)]
+pub struct HexagonalError {
+    message: String,
+}
+
+impl Display for HexagonalError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for HexagonalError {
+
+}
+
+impl HexagonalError {
+    pub fn new(message: String) -> Self {
+        HexagonalError { message }
+    }
+}
+
+pub type HexagonalResult<T> = Result<T, HexagonalError>;
